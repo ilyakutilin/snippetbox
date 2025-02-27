0a0b525870076d630df9983591d787594766bcd9
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-02-27 15:09:47 +0300

    Routes setup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -39,6 +39,21 @@ func (app *application) routes() http.Handler {
 		dynamic.ThenFunc(app.snippetCreatePost),
 	)
 
+	// Auth-related routes
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(
+		http.MethodPost,
+		"/user/signup",
+		dynamic.ThenFunc(app.userSignupPost),
+	)
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(
+		http.MethodPost,
+		"/user/logout",
+		dynamic.ThenFunc(app.userLogoutPost),
+	)
+
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
 	return standard.Then(router)
