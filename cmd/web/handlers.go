18390a0e1ade53a067961d73156831fcb39b5b18
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-02-27 16:11:51 +0300

    User input validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,14 +19,14 @@ type snippetCreateForm struct {
 	Title               string `form:"title"`
 	Content             string `form:"content"`
 	Expires             int    `form:"expires"`
-	validator.Validator `form:"-"`
+	validator.Validator `       form:"-"`
 }
 
 type userSignupForm struct {
 	Name                string `form:"name"`
 	Email               string `form:"email"`
 	Password            string `form:"password"`
-	validator.Validator `form:"-"`
+	validator.Validator `       form:"-"`
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -145,6 +145,46 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	var form userSignupForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Validate the form
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(
+		validator.NotBlank(form.Email),
+		"email",
+		"This field cannot be blank",
+	)
+	form.CheckField(
+		validator.Matches(form.Email, validator.EmailRX),
+		"email",
+		"This field must be a valid email address",
+	)
+	form.CheckField(
+		validator.NotBlank(form.Password),
+		"password",
+		"This field cannot be blank",
+	)
+	form.CheckField(
+		validator.MinChars(form.Password, 8),
+		"password",
+		"This field must be at least 8 characters long",
+	)
+
+	// If there are any errors, redisplay the signup form along with a 422 status code.
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		return
+	}
+
+	// Otherwise send the placeholder response (for now!).
 	fmt.Fprintln(w, "Create a new user...")
 }
 
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,9 +1,14 @@
 package validator
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
+)
+
+var EmailRX = regexp.MustCompile(
+	"^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
 )
 
 type Validator struct {
@@ -38,6 +43,11 @@ func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
+// MinChars() returns true if a value contains at least n characters.
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
 // MaxChars() returns true if a value contains no more than n characters.
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
@@ -46,4 +56,10 @@ func MaxChars(value string, n int) bool {
 // PermittedInt() returns true if a value is in a list of permitted integers.
 func PermittedInt(value int, permittedValues ...int) bool {
 	return slices.Contains(permittedValues, value)
+}
+
+// Matches() returns true if a value matches a provided compiled regular
+// expression pattern.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
